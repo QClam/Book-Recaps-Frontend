079786712ce192fad6de2652ff7ee00e4779c8cf
Xuan Quy <hoangxuanquy5@gmail.com>
2024-10-15 21:04:02 +0700

    Admin - Change Role function

--- book_recaps_admin/src/Components/Users/UserList.jsx
+++ book_recaps_admin/src/Components/Users/UserList.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Hourglass } from "react-loader-spinner";
-import { Add } from "@mui/icons-material";
+import { Add, MoreHoriz } from "@mui/icons-material";
 import Modal from "react-modal";
 import Swal from "sweetalert2";
+import Pagination from "@mui/material/Pagination";
+import { Button, Menu, MenuItem } from "@mui/material";
 
-import api from '../Auth/AxiosInterceptors'
+import api from "../Auth/AxiosInterceptors";
 import "./UserList.scss";
 import "../Loading.scss";
-import Pagination from '@mui/material/Pagination';
+import avatar from "../../data/avarta.png"
 
 Modal.setAppElement("#root"); // Set the root element for the modal
 
@@ -29,19 +31,21 @@ function UsersList() {
     phoneNumber: "",
   });
   const [editingUserId, setEditingUserId] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null); // State để lưu anchor của submenu
+  const [submenuAnchorEl, setSubmenuAnchorEl] = useState(null); // State cho submenu lồng
+  const [submenuOpen, setSubmenuOpen] = useState(false); // Trạng thái để kiểm soát submenu
+  const [selectedUser, setSelectedUser] = useState(null); // State để lưu user đã chọn
 
-  const token = localStorage.getItem('access_token');
+  const token = localStorage.getItem("access_token");
 
   const fetchUsers = async () => {
     try {
-      const response = await api.get('/users/getalluser',
-        {
-          headers: {
-            'accept': "*/*",
-            Authorization: `Bearer ${token}`
-          }
-        }
-      )
+      const response = await api.get("/users/getalluser", {
+        headers: {
+          accept: "*/*",
+          Authorization: `Bearer ${token}`,
+        },
+      });
       setUsers(response.data.$values);
       console.log("Users: ", response.data);
     } catch (error) {
@@ -55,7 +59,16 @@ function UsersList() {
     fetchUsers();
   }, []);
 
-  const displayUsers = users.slice((currentPage - 1) * usersPerPage, currentPage * usersPerPage); // Adjust slicing for 1-based page indexing
+  useEffect(() => {
+    if (currentPage > Math.ceil(users.length / usersPerPage)) {
+      setCurrentPage(1);
+    }
+  }, [users.length, currentPage]);
+
+  const displayUsers = users.slice(
+    (currentPage - 1) * usersPerPage,
+    currentPage * usersPerPage
+  ); // Adjust slicing for 1-based page indexing
   const handlePageChange = (event, value) => {
     setCurrentPage(value);
   };
@@ -104,6 +117,7 @@ function UsersList() {
     });
     setModalIsOpen(false);
     setError(null);
+    setEditingUserId(null); // Reset user đang chỉnh sửa khi đóng modal
   };
 
   const handleAddUser = async () => {
@@ -123,7 +137,7 @@ function UsersList() {
         newUser
       );
       setUsers([...users, response.data]); // Update user list
-      resetForm()
+      resetForm();
     } catch (error) {
       console.error("Error registering user:", error);
       setError("Đăng ký thất bại.");
@@ -155,6 +169,64 @@ function UsersList() {
     }
   };
 
+  const handleChangeRole = async (userId, newRole) => {
+    Swal.fire({
+      title: "Bạn có chắc chắn muốn thay đổi?",
+      text: "Bạn sẽ cấp quyền cho người dùng này!",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Thay đổi",
+      cancelButtonText: "Hủy",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const response = await axios.put(
+            `https://160.25.80.100:7124/api/update-user-role/${newRole}`,
+            null, // No body content needed
+            {
+              params: { userId: userId }, // Adding userId as a query parameter
+              headers: {
+                accept: "*/*",
+                Authorization: `Bearer ${token}`, // Token xác thực
+              },
+            }
+          );
+
+          if (response && response.status === 200) {
+            // Update the user's role in the local state
+            setUsers((prevUsers) =>
+              prevUsers.map((user) =>
+                user.userId === userId ? { ...user, roleType: newRole } : user
+              )
+            );
+            Swal.fire(
+              "Đã cập nhật!",
+              "Người dùng đã được cấp quyền mới",
+              "success"
+            );
+            fetchUsers();
+          } else {
+            console.error("Unexpected response:", response);
+            Swal.fire(
+              "Thất bại",
+              "Có lỗi xảy ra trong quá trình cập nhật",
+              "error"
+            );
+          }
+        } catch (error) {
+          console.log("Failed to Change Role", error);
+          Swal.fire(
+            "Thất bại",
+            "Có lỗi xảy ra trong quá trình cập nhật",
+            "error"
+          );
+        }
+      }
+    });
+  };
+
   const handleDeleteUser = async (userId) => {
     Swal.fire({
       title: "Bạn có chắc chắn muốn xóa?",
@@ -180,7 +252,7 @@ function UsersList() {
               },
             }
           );
-          
+
           if (response && response.status === 200) {
             setUsers(users.filter((user) => user.userId !== userId));
             Swal.fire("Đã xóa!", "Người dùng đã được xóa", "success");
@@ -197,13 +269,33 @@ function UsersList() {
     });
   };
 
+  const handleMenuOpen = (event, user) => {
+    setAnchorEl(event.currentTarget); // Đặt anchor của menu
+    setSelectedUser(user); // Lưu thông tin user đã chọn
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null); // Đóng menu
+    setSelectedUser(null);
+  };
+
+  const handleSubmenu = (event) => {
+    setSubmenuAnchorEl(event.currentTarget);
+    setSubmenuOpen(true);
+  };
+
+  const handleSubmenuClose = () => {
+    setSubmenuAnchorEl(null);
+    setSubmenuOpen(false);
+  };
+
   const openEditModal = (user) => {
     setRegisterForm({
       fullName: user.fullName,
       email: user.email,
-      password: user.password,
-      confirmPassword: user.confirmPassword,
+      birthDate: user.birthDate,
       phoneNumber: user.phoneNumber,
+      address: user.address,
     });
     setEditingUserId(user.id);
     setModalIsOpen(true);
@@ -241,7 +333,9 @@ function UsersList() {
         className="modal"
         overlayClassName="modal-overlay"
       >
-        <h2>{editingUserId ? "Cập nhật người dùng" : "Thêm người dùng"}</h2>
+        <h2 style={{ color: "#1a1a1a" }}>
+          {editingUserId ? "Cập nhật người dùng" : "Thêm người dùng"}
+        </h2>
         <form>
           <div className="input-group">
             <input
@@ -257,27 +351,30 @@ function UsersList() {
               placeholder="Số điện thoại"
               value={registerForm.phoneNumber}
               onChange={(e) =>
-                setRegisterForm({ ...registerForm, phoneNumber: e.target.value })
+                setRegisterForm({
+                  ...registerForm,
+                  phoneNumber: e.target.value,
+                })
               }
             />
           </div>
           <div className="input-group">
             <input
-              type="password"
-              placeholder="Mật khẩu"
-              value={registerForm.password}
+              type="date"
+              placeholder="Ngày sinh"
+              value={registerForm.birthDate}
               onChange={(e) =>
-                setRegisterForm({ ...registerForm, password: e.target.value })
+                setRegisterForm({ ...registerForm, birthDate: e.target.value })
               }
             />
             <input
-              type="password"
-              placeholder="Xác nhận mật khẩu"
-              value={registerForm.confirmPassword}
+              type="text"
+              placeholder="Địa chỉ"
+              value={registerForm.address}
               onChange={(e) =>
                 setRegisterForm({
                   ...registerForm,
-                  confirmPassword: e.target.value,
+                  address: e.target.value,
                 })
               }
             />
@@ -319,73 +416,154 @@ function UsersList() {
               <th>Ảnh Đại Diện</th>
               <th>Số Điện Thoại</th>
               <th>Ngày Sinh</th>
-              <th></th>
+              <th>Vai trò</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {displayUsers.map((val) => (
-              <tr key={val.id}>
-                <td>{val.fullName}</td>
-                <td>{val.email}</td>
+            {displayUsers.map((user) => (
+              <tr key={user.id}>
+                <td>{user.fullName}</td>
+                <td>{user.email}</td>
                 <td>
                   <img
-                    src={val.imageUrl}
+                    src={avatar}
+                    alt="avatar"
+                    style={{ width: 70, height: 70 }}
+                  />
+                  {/* <img
+                    src={user.imageUrl}
                     alt="avatar"
                     style={{ width: 70, height: 60 }}
+                  /> */}
+                </td>
+                <td>{user.phoneNumber}</td>
+                <td>{user.birthDate}</td>
+                <td>
+                  {user.roleType === 0 ? (
+                    <button style={{ backgroundColor: "green" }}>
+                      Super Admin
+                    </button>
+                  ) : user.roleType === 1 ? (
+                    <button style={{ backgroundColor: "#007bff" }}>
+                      Staff
+                    </button>
+                  ) : user.roleType === 2 ? (
+                    <button style={{ backgroundColor: "#1d96c6" }}>
+                      Contributor
+                    </button>
+                  ) : user.roleType === 3 ? (
+                    <button style={{ backgroundColor: "#268a3a" }}>
+                      Publisher
+                    </button>
+                  ) : user.roleType === 4 ? (
+                    <button style={{ backgroundColor: "#a74f19" }}>
+                      Customer
+                    </button>
+                  ) : (
+                    <button style={{ backgroundColor: "#5e6061" }}>
+                      Unknow Role
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <Button
+                    startIcon={<MoreHoriz />}
+                    onClick={(event) => handleMenuOpen(event, user)}
                   />
-                </td>
-                <td>{val.phoneNumber}</td>
-                <td>{val.birthDate}</td>
-                <td>
-                  <button
-                    className="button"
-                    style={{ backgroundColor: "green" }}
-                    onClick={() => openEditModal(val)}
-                  >
-                    Cập nhật
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="button"
-                    style={{ backgroundColor: "red" }}
-                    onClick={() => handleDeleteUser(val.id)}
-                  >
-                    Xóa
-                  </button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      
-        <Pagination
-          className='center'
-          count={Math.ceil(users.length / usersPerPage)} // Total number of pages
-          page={currentPage} // Current page
-          onChange={handlePageChange} // Handle page change
-          color="primary" // Styling options
-          showFirstButton
-          showLastButton
-          sx={{
-            '& .MuiPaginationItem-root': { 
-                color: isDarkMode ? '#fff' : '#000', // Change text color based on theme
-                backgroundColor: isDarkMode ? '#555' : '#f0f0f0', // Button background color based on theme
-            },
-            '& .MuiPaginationItem-root.Mui-selected': { 
-                backgroundColor: isDarkMode ? '#306cce' : '#72a1ed', // Change color of selected page button
-                color: '#fff', // Ensure selected text is white for contrast
-            },
-            '& .MuiPaginationItem-root.Mui-selected:hover': {
-                backgroundColor: isDarkMode ? '#2057a4' : '#5698d3', // Color on hover for selected button
-            },
-            '& .MuiPaginationItem-root:hover': {
-                backgroundColor: isDarkMode ? '#666' : '#e0e0e0', // Color on hover for non-selected buttons
-            },
+
+      <Pagination
+        className="center"
+        count={Math.ceil(users.length / usersPerPage)} // Total number of pages
+        page={currentPage} // Current page
+        onChange={handlePageChange} // Handle page change
+        color="primary" // Styling options
+        showFirstButton
+        showLastButton
+        sx={{
+          "& .MuiPaginationItem-root": {
+            color: isDarkMode ? "#fff" : "#000", // Change text color based on theme
+            backgroundColor: isDarkMode ? "#555" : "#f0f0f0", // Button background color based on theme
+          },
+          "& .MuiPaginationItem-root.Mui-selected": {
+            backgroundColor: isDarkMode ? "#306cce" : "#72a1ed", // Change color of selected page button
+            color: "#fff", // Ensure selected text is white for contrast
+          },
+          "& .MuiPaginationItem-root.Mui-selected:hover": {
+            backgroundColor: isDarkMode ? "#2057a4" : "#5698d3", // Color on hover for selected button
+          },
+          "& .MuiPaginationItem-root:hover": {
+            backgroundColor: isDarkMode ? "#666" : "#e0e0e0", // Color on hover for non-selected buttons
+          },
         }}
-        />
+      />
+
+      {/* Submenu */}
+      <Menu
+        anchorEl={anchorEl} // Menu anchor
+        open={Boolean(anchorEl)} // Menu is open if anchorEl is set
+        onClose={handleMenuClose}
+      >
+        <MenuItem onClick={handleSubmenu}>Thay đổi Vai trò</MenuItem>
+        <Menu
+          anchorEl={submenuAnchorEl}
+          open={submenuOpen}
+          onClose={handleSubmenuClose}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }} // Vị trí của submenu gốc từ bên trái
+          transformOrigin={{ vertical: "top", horizontal: "left" }} // Submenu xuất hiện bên trái
+        >
+          <MenuItem
+            onClick={() => {
+              handleSubmenuClose();
+              handleMenuClose();
+              handleChangeRole(selectedUser.id, 1);
+            }}
+          >
+            Staff
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              handleSubmenuClose();
+              handleMenuClose();
+              handleChangeRole(selectedUser.id, 2);
+            }}
+          >
+            Contributor
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              handleSubmenuClose();
+              handleMenuClose();
+              handleChangeRole(selectedUser.id, 3);
+            }}
+          >
+            Publisher
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              handleSubmenuClose();
+              handleMenuClose();
+              handleChangeRole(selectedUser.id, 4);
+            }}
+          >
+            Customer
+          </MenuItem>
+        </Menu>
+        <MenuItem
+          onClick={() => {
+            handleMenuClose();
+            handleDeleteUser(selectedUser.id); // Delete the selected user
+          }}
+        >
+          Xóa
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
