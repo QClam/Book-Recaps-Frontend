ac0fc2f5ebc21ece568a92ea6a98b303ba5e3889
Xuan Quy <hoangxuanquy5@gmail.com>
2024-11-25 00:25:35 +0700

    Dashboard Data

--- book_recaps_admin/src/Components/Dashboard/Dashboard.jsx
+++ book_recaps_admin/src/Components/Dashboard/Dashboard.jsx
@@ -1,28 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DateRangePicker } from 'rsuite';
 import 'rsuite/dist/rsuite.min.css';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { Typography, Box } from '@mui/material';
 import { BarChart, LocalOffer, Article, RemoveRedEye, AttachMoney } from '@mui/icons-material';
+import dayjs from 'dayjs';
 
 import './Dashboard.scss';
+import api from '../Auth/AxiosInterceptors';
 
 function Dashboard() {
+
+    const today = dayjs().format("YYYY-MM-DD");
+    const [dateRange, setDateRange] = useState([today, today]);
+
+    const [dashboardData, setDashboardData] = useState([]);
+    const [premiumPackage, setPremiumPackage] = useState([]);
+
+    const fetchDashboardData = async (fromDate, toDate) => {
+        try {
+            const response = await api.get('/api/dashboard/admindashboard', {
+                params: { fromDate, toDate },
+            });
+            const data = response.data.data || [];
+            const premiumPackage = data.packageSales.$values;
+            setDashboardData(data);
+            setPremiumPackage(premiumPackage);
+            console.log("DashboardData: ", data);
+            console.log("Package: ", premiumPackage);
+
+        } catch (error) {
+            console.error('Lỗi khi gọi API:', error);
+        }
+    };
+
+    useEffect(() => {
+        const fromDate = dayjs(today).format("YYYY-MM-DD");
+        const toDate = dayjs(today).format("YYYY-MM-DD");
+        fetchDashboardData(fromDate, toDate);
+    }, []); // Chỉ chạy khi component được render lần đầu
+
+    const handleDateChange = async (range) => {
+        setDateRange(range);
+
+        if (range[0] && range[1]) {
+            const fromDate = dayjs(range[0]).format("YYYY-MM-DD");
+            const toDate = dayjs(range[1]).format("YYYY-MM-DD");
+            console.log('From Date:', fromDate, 'To Date:', toDate);
+
+            fetchDashboardData(fromDate, toDate);
+        }
+    };
+
     const overview = [
-        { title: 'Doanh thu gói Package', value: 24000000 },
-        { title: 'Số lượng Package đã bán', value: 1200 },
-        { title: 'Số bài viết mới', value: 20 },
+        { title: 'Doanh thu gói Package', value: dashboardData?.revenueFromPackages || "Chưa tính toán" },
+        { title: 'Số lượng Gói Premium đã bán (Theo tháng)', value: premiumPackage[0]?.count || "Chưa tính toán" },
+        { title: 'Số lượng Gói Premium đã bán (Theo năm)', value: premiumPackage[1]?.count || "Chưa tính toán" },
+        { title: 'Số bài viết mới', value: dashboardData?.newRecaps || "Chưa tính toán" },
+        { title: 'Tiền chi cho Nhà xuất bản', value: dashboardData.publisherExpense || "Chưa tính toán" },
+        { title: 'Tiền chi cho Người đóng góp', value: dashboardData.contributorExpense || "Chưa tính toán" },
     ];
     const views = [
-        { title: 'Tổng lượt View', value: 18247 },
-        { title: 'Doanh thu từ lượt View', value: 21000000 },
-        { title: 'Lợi nhuận từ lượt View', value: 1000000 },
-        { title: 'Tiền chi cho Publisher', value: 12000000 },
-        { title: 'Tiền chi cho Contributor', value: 9000000 },
+        { title: 'Tổng lượt View', value: dashboardData.totalViews || "Chưa tính toán" },
+        { title: 'Doanh thu từ lượt View', value: dashboardData.revenueFromViews || "Chưa tính toán" },
+        { title: 'Lợi nhuận từ lượt View', value: dashboardData.platformProfit || "Chưa tính toán" },
     ];
     const platform = [
-        { title: 'Số dư hiện tại', value: 39000000 },
+        { title: 'Số dư hiện tại', value: dashboardData.currentBalance || "Chưa tính toán" },
+        { title: 'Tổng thu được', value: dashboardData.totalRevenue || "Chưa tính toán" },
+        { title: 'Tổng đã chi', value: dashboardData.totalExpenses || "Chưa tính toán" },
     ];
 
     const getIcon = (title) => {
@@ -44,7 +91,11 @@ function Dashboard() {
 
     const getUnit = (title) => {
         if (title.includes('Tổng lượt View')) return 'Views';
-        if (title.includes('Doanh thu') || title.includes('Lợi nhuận') || title.includes('Tiền chi') || title.includes('Số dư')) return 'VND';
+        if (title.includes('Doanh thu') ||
+            title.includes('Lợi nhuận') ||
+            title.includes('Tiền chi') ||
+            title.includes('Số dư') ||
+            title.includes('Tổng')) return 'VND';
         if (title.includes('Package')) return 'Gói'
         if (title.includes('bài viết')) return 'Bài'
         return '';
@@ -56,7 +107,7 @@ function Dashboard() {
                 <Paper elevation={3} sx={{ padding: 2, position: 'relative' }}>
                     <Typography variant="subtitle1">{item.title}</Typography>
                     <Typography variant="h5">
-                        {item.value.toLocaleString()} {getUnit(item.title)}
+                        {(item.value ?? 0).toLocaleString('vi-VN')} {getUnit(item.title)}
                     </Typography>
                     {getIcon(item.title)}
                 </Paper>
@@ -68,9 +119,10 @@ function Dashboard() {
         <div className='dashboard-container'>
             <div className='header'>
                 <div className="date-picker-container">
-                    <div className="date-picker">
-                        <DateRangePicker />
-                    </div>
+                    <Typography variant='body' textAlign='center' sx={{ marginBottom: 2 }}>Hãy chọn khoảng thời gian để hiển thị số liệu</Typography>
+                    <DateRangePicker
+                        onChange={handleDateChange}
+                    />
                 </div>
             </div>
 
@@ -85,7 +137,7 @@ function Dashboard() {
                 </div>
 
                 <div className="view">
-                    <h4>Views</h4>
+                    <h4>Lượt xem</h4>
                     <Box sx={{ flexGrow: 1, padding: 2 }}>
                         <Grid container spacing={3} columns={{ xs: 4, sm: 8, md: 12 }}>
                             {renderDataCards(views)}
