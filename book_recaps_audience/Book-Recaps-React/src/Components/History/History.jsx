699372f9799515ea6ac94bfc0033560eacd0c585
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-12-18 21:22:36 +0700

    Update History.jsx

--- book_recaps_audience/Book-Recaps-React/src/Components/History/History.jsx
+++ book_recaps_audience/Book-Recaps-React/src/Components/History/History.jsx
@@ -56,88 +56,94 @@ const History = () => {
     <div className="container mx-auto max-w-screen-xl p-5">
       <div className="history-container">
         <h1>Lịch sử xem bài viết</h1>
-        <div className="history-list">
-          {viewTrackings.map((view, index) => (
-            <Link
-              key={index}
-              to={generatePath(routes.recapPlayer, { recapId: view.recapId })}
-              className="history-card"
-            >
-              <div className="relative w-28">
-                <Image
-                  src={view.book.coverImage || "/empty-image.jpg"}
-                  alt={view.book.title}
-                  className="!block overflow-hidden rounded-md shadow-md w-full"
-                  imageClassName="aspect-[3/4] object-cover w-full bg-white"
-                />
-              </div>
+        <Show when={viewTrackings.length > 0} fallback={
+          <p className="text-center text-gray-500 italic">
+            Bạn chưa xem bài viết nào
+          </p>
+        }>
+          <div className="history-list">
+            {viewTrackings.map((view, index) => (
+              <Link
+                key={index}
+                to={generatePath(routes.recapPlayer, { recapId: view.recapId })}
+                className="history-card"
+              >
+                <div className="relative w-28">
+                  <Image
+                    src={view.book.coverImage || "/empty-image.jpg"}
+                    alt={view.book.title}
+                    className="!block overflow-hidden rounded-md shadow-md w-full"
+                    imageClassName="aspect-[3/4] object-cover w-full bg-white"
+                  />
+                </div>
 
-              <div className="flex-1">
-                <p className="text-base mb-2 line-clamp-1 flex gap-2 items-center" title={view.recapName}>
-                  <span>Bài viết: <strong>{view.recapName}</strong></span>
-                  <Show when={view.isPremium}>
-                    <span className="bg-yellow-400 text-xs rounded px-2 py-1">Premium</span>
-                  </Show>
-                </p>
-                <div className="flex gap-2 items-center text-sm mb-2">
-                  <div className="w-6 h-6">
-                    <img
-                      src={view.contributorImage?.replace("Files/Image/jpg/ad.jpg", "") || '/avatar-placeholder.png'}
-                      alt="User Avatar" className="w-full h-full object-cover rounded-full"/>
+                <div className="flex-1">
+                  <p className="text-base mb-2 line-clamp-1 flex gap-2 items-center" title={view.recapName}>
+                    <span>Bài viết: <strong>{view.recapName}</strong></span>
+                    <Show when={view.isPremium}>
+                      <span className="bg-yellow-400 text-xs rounded px-2 py-1">Premium</span>
+                    </Show>
+                  </p>
+                  <div className="flex gap-2 items-center text-sm mb-2">
+                    <div className="w-6 h-6">
+                      <img
+                        src={view.contributorImage?.replace("Files/Image/jpg/ad.jpg", "") || '/avatar-placeholder.png'}
+                        alt="User Avatar" className="w-full h-full object-cover rounded-full"/>
+                    </div>
+                    <p className="font-semibold line-clamp-1">{view.contributorName}</p>
                   </div>
-                  <p className="font-semibold line-clamp-1">{view.contributorName}</p>
+
+                  <div className="flex gap-2 items-center text-sm text-gray-500 mb-2">
+                    {/*<p className="flex items-center gap-2">*/}
+                    {/*  <span className="bg-green-100 p-1 rounded"><RiEyeLine size={15}/></span>*/}
+                    {/*  <span>{view.viewsCount || 0} Lượt xem</span>*/}
+                    {/*</p>*/}
+                    {/*<p>·</p>*/}
+                    {/*<p className="flex items-center gap-2">*/}
+                    {/*  <span className="bg-green-100 p-1 rounded"><RiThumbUpLine size={15}/></span>*/}
+                    {/*  <span>{view.likesCount || 0} Lượt thích</span>*/}
+                    {/*</p>*/}
+                    {/*<p>·</p>*/}
+                    <p className="flex items-center gap-2">
+                      <span className="bg-green-100 p-1 rounded"><FaClockRotateLeft size={15}/></span>
+                      <span>{new Date(view.createdAt + "Z").toLocaleString()}</span>
+                    </p>
+                    <p>·</p>
+                    <p className="flex items-center gap-2">
+                      <span className="bg-green-100 p-1 rounded"><LuTimer size={15}/></span>
+                      <span>Đã xem {((view.durations || 0) / 60).toFixed(1).replace(/(\.0)$/, '')} phút</span>
+                    </p>
+                  </div>
+
+                  <div className="border-t border-gray-300 my-2.5"></div>
+
+                  <div className="relative text-xs italic">
+                    <p className="mb-1 line-clamp-2" title={view.book.originalTitle}>
+                      Sách: <strong>{view.book.originalTitle} ({view.book.title})</strong>
+                    </p>
+                    <p className="line-clamp-1"
+                       title={view.book.authors.$values.join(', ')}>
+                      Tác giả: <strong>{view.book.authors.$values.join(', ')}</strong>
+                    </p>
+                  </div>
                 </div>
+              </Link>
+            ))}
+          </div>
 
-                <div className="flex gap-2 items-center text-sm text-gray-500 mb-2">
-                  {/*<p className="flex items-center gap-2">*/}
-                  {/*  <span className="bg-green-100 p-1 rounded"><RiEyeLine size={15}/></span>*/}
-                  {/*  <span>{view.viewsCount || 0} Lượt xem</span>*/}
-                  {/*</p>*/}
-                  {/*<p>·</p>*/}
-                  {/*<p className="flex items-center gap-2">*/}
-                  {/*  <span className="bg-green-100 p-1 rounded"><RiThumbUpLine size={15}/></span>*/}
-                  {/*  <span>{view.likesCount || 0} Lượt thích</span>*/}
-                  {/*</p>*/}
-                  {/*<p>·</p>*/}
-                  <p className="flex items-center gap-2">
-                    <span className="bg-green-100 p-1 rounded"><FaClockRotateLeft size={15}/></span>
-                    <span>{new Date(view.createdAt + "Z").toLocaleString()}</span>
-                  </p>
-                  <p>·</p>
-                  <p className="flex items-center gap-2">
-                    <span className="bg-green-100 p-1 rounded"><LuTimer size={15}/></span>
-                    <span>Đã xem {((view.durations || 0) / 60).toFixed(1).replace(/(\.0)$/, '')} phút</span>
-                  </p>
-                </div>
-
-                <div className="border-t border-gray-300 my-2.5"></div>
-
-                <div className="relative text-xs italic">
-                  <p className="mb-1 line-clamp-2" title={view.book.originalTitle}>
-                    Sách: <strong>{view.book.originalTitle} ({view.book.title})</strong>
-                  </p>
-                  <p className="line-clamp-1"
-                     title={view.book.authors.$values.join(', ')}>
-                    Tác giả: <strong>{view.book.authors.$values.join(', ')}</strong>
-                  </p>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-
-        <ReactPaginate
-          previousLabel="Trước"
-          nextLabel="Sau"
-          breakLabel="..."
-          pageCount={totalPages.current}
-          initialPage={page - 1}
-          marginPagesDisplayed={1}
-          pageRangeDisplayed={1}
-          onPageChange={handleChangePage}
-          containerClassName="pagination"
-          activeClassName="active"
-        />
+          <ReactPaginate
+            previousLabel="Trước"
+            nextLabel="Sau"
+            breakLabel="..."
+            pageCount={totalPages.current}
+            initialPage={page - 1}
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={1}
+            onPageChange={handleChangePage}
+            containerClassName="pagination"
+            activeClassName="active"
+          />
+        </Show>
       </div>
     </div>
   );
